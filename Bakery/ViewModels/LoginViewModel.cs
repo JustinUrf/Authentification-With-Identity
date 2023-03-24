d5bc36a033aca8528f95dd1adbefba1f5cfca343
Justin Lee <lee.justin001126@gmail.com>
2023-03-24 07:00:02 -0700

    Add login and register view models

---
+++ Bakery/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+namespace Bakery.ViewModels
+{
+  public class LoginViewModel
+  {
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email Address")]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+  }
+}
