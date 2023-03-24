f501668780d303199bb3c60ecd1f73d918492dab
Justin Lee <lee.justin001126@gmail.com>
2023-03-24 11:01:28 -0700

    add Detail controller logic

--- Bakery/Controllers/FlavorsController.cs
+++ Bakery/Controllers/FlavorsController.cs
@@ -44,7 +44,16 @@ namespace Bakery.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
       }
-
     }
+
+    public ActionResult Details(int id)
+    {
+      Flavor thisFlavor = _db.Flavors
+                          .Include(flavor => flavor.JoinEntities)
+                          .ThenInclude(join => join.Treat)
+                          .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
   }
 }
