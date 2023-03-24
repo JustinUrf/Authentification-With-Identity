642191f15ded444a16a4b190a6a07583cba419ba
Justin Lee <lee.justin001126@gmail.com>
2023-03-24 08:20:00 -0700

    Add all neccesassary models to projects

--- Bakery/Models/TreatFlavor.cs
+++ Bakery/Models/TreatFlavor.cs
@@ -2,6 +2,11 @@ namespace Bakery.Models
 {
   public class TreatFlavor
   {
-
+    public int TreatFlavorId { get; set; }
+    public int TreatId { get; set; }
+    public Treat Treat { get; set; }
+    public int FlavorId { get; set; }
+    public Flavor Flavor { get; set; }
+    
   }
 }
