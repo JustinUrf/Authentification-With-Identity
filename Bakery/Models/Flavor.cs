02418d5a6df93f4e867443107e2cf64d7989d56c
Justin Lee <lee.justin001126@gmail.com>
2023-03-24 08:17:25 -0700

    Add treat and flavor models

--- Bakery/Models/Flavor.cs
+++ Bakery/Models/Flavor.cs
@@ -5,6 +5,11 @@ namespace Bakery.Models
 {
   public class Flavor
   {
+    public int FlavorId { get; set; }
+    
+    [Required(ErrorMessage = "You must name this flavor!")]
+    public string FlavorName { get; set; }
+    public List<TreatFlavor> JoinEntities { get; }
 
   }
 }
--- Bakery/Models/Treat.cs
+++ Bakery/Models/Treat.cs
@@ -5,6 +5,10 @@ namespace Bakery.Models
 {
   public class Treat
   {
-
+    public int TreatId { get; set; }
+    
+    [Required(ErrorMessage = "You must name this treat!")]
+    public string TreatName { get; set; }
+    public List<TreatFlavor> JoinEntities { get; }
   }
 }
